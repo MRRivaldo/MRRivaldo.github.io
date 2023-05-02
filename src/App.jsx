f6906193bcb0234fb405c33bd5b2af15c2102a91
Rivaldo <rivaldorizky77@gmail.com>
2023-05-02 15:31:45 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <>
-      <Router basename="/mrrivaldo.github.io">
+      <Router>
         <Navigationbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
