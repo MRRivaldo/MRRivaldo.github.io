eaadd3ecc42368b37ab77e190c6974c206a6c94d
MR Rivaldo <85377746+mrrivaldo@users.noreply.github.com>
2023-07-13 12:21:45 +0800

    Update FirstSection.jsx

--- src/components/FirstSection.jsx
+++ src/components/FirstSection.jsx
@@ -7,7 +7,7 @@ function FirstSection() {
     <Container>
       <section className="first-section">
         <div className="container-section">
-          <h1 className="title">Passionate Frontend Developer & Cybersecurity Enthusiast</h1>
+          <h1 className="title">Frontend Developer & Cybersecurity Enthusiast</h1>
           <h2 className="subtitle">Exploring the Fascinating Intersection of Code and Security</h2>
           <img className="avatar" src="/images/avataaars.svg" />
         </div>
